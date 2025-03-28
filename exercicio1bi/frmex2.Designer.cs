ddf04052375813350ad37335ef432a79372aed5c
Laura <lauxavi1357@gmail.com>
2025-03-28 10:46:19 -0300

    Fazendo exercício 2

---
+++ exercicio1bi/frmex2.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace exercicio1bi
+{
+    partial class frmex2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.btnMedia = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMédia = new System.Windows.Forms.TextBox();
+            this.txtNota = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInserir.Location = new System.Drawing.Point(209, 330);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(75, 23);
+            this.btnInserir.TabIndex = 0;
+            this.btnInserir.Text = "inserir nota";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // btnMedia
+            // 
+            this.btnMedia.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMedia.Location = new System.Drawing.Point(525, 330);
+            this.btnMedia.Name = "btnMedia";
+            this.btnMedia.Size = new System.Drawing.Size(123, 23);
+            this.btnMedia.TabIndex = 1;
+            this.btnMedia.Text = "calcular média";
+            this.btnMedia.UseVisualStyleBackColor = true;
+            this.btnMedia.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(211, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "nota";
+            // 
+            // txtMédia
+            // 
+            this.txtMédia.Location = new System.Drawing.Point(561, 116);
+            this.txtMédia.Name = "txtMédia";
+            this.txtMédia.Size = new System.Drawing.Size(100, 20);
+            this.txtMédia.TabIndex = 4;
+            // 
+            // txtNota
+            // 
+            this.txtNota.Location = new System.Drawing.Point(184, 116);
+            this.txtNota.Name = "txtNota";
+            this.txtNota.Size = new System.Drawing.Size(100, 20);
+            this.txtNota.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(586, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "média";
+            // 
+            // frmex2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNota);
+            this.Controls.Add(this.txtMédia);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnMedia);
+            this.Controls.Add(this.btnInserir);
+            this.Name = "frmex2";
+            this.Text = "Exercício 2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Button btnMedia;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMédia;
+        private System.Windows.Forms.TextBox txtNota;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ exercicio1bi/frmex2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exercicio1bi
+{
+    public partial class frmex2: Form
+    {    
+        double[] notas = new double[4];
+
+        public frmex2()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double media;
+            int i;
+            for (i=0; i<4; i++) {
+                media += notas[i];
+            }
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            
+            double nota;
+
+            for (int i=0; i<4; i++) {
+                nota = Convert.ToDouble(txtNota.Text);
+                notas[i] = nota;
+            }
+        }
+    }
+}
